33a38fce3d1694bbc2d015158a0ab256e61d6504
Kichan Park <kicpark@c1r3s4.42seoul.kr>
2021-03-05 01:10:02 +0900

    asdf

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kicpark <emmet.urssu@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 10:33:14 by kicpark           #+#    #+#             */
-/*   Updated: 2021/03/02 10:33:14 by kicpark          ###   ########.fr       */
+/*   Updated: 2021/03/05 01:08:27 by kicpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@
 
 int		get_next_line(int fd, char **line);
 
-char    *ft_strchr(char *s, int c);
-int ft_strlen(char *c);
+char	*ft_strchr(char *s, int c);
+int		ft_strlen(char *c);
 char	*ft_strcpy(char	*dst, char *stc, int n);
 char	*ft_strndup(char *s1, int size);
 char	*ft_strjoin(char *s1, char *s2);
